e346eb817e9640a607fad948e03dca00e2dcb19b
Mahesh Kale <kjyotiba@cs.iitr.ac.in>
2019-03-07 21:05:21 +0530

    Add a run function that executes commands from JAIL

--- container.cpp
+++ container.cpp
@@ -1,27 +1,42 @@
-#include <iostream>         // Input output stream
-#include <sched.h>          // Contain clone syscall for cloning a process
-#include <signal.h>         // Contain ANSI C signal handeling like SIGCHILD   
+#include <iostream> // Input output stream
+#include <sched.h>  // Contain clone syscall for cloning a process
+#include <unistd.h> // Contain execvp function
+#include <wait.h>   // Contain ANSI C signal handeling like SIGCHILD
 
-char* stack_memory() {
-  const int stackSize = 65536;
-  auto *stack = new (std::nothrow) char[stackSize];
+char *stack_memory()
+{
+    const int stackSize = 65536;
+    auto *stack = new (std::nothrow) char[stackSize];
 
-  if (stack == nullptr) {                       // Can't allocate memory from heap hence `new` will return null pointer
-    printf("Memory allocation failed !! \n");
-    exit(1);
-  }  
+    if (stack == nullptr)
+    { // Can't allocate memory from heap hence `new` will return null pointer
+        printf("Memory allocation failed !! \n");
+        exit(1);
+    }
 
-  return stack+stackSize;  // As stack grows backward so we have to start from end of stack   {(end of stack)| *stack|-------> stackSize(start of stack)}
+    return stack + stackSize; // As stack grows backward so we have to start from end of stack   {(end of stack)| *stack|-------> stackSize(start of stack)}
 }
 
-int jail(void *args) {                              // Function JAIL that is container 
-  printf("Namastey from JAIL \n");
-  return 0;
+template <typename P>                   
+int run(P params)
+{
+    //basically generating the arguments array at compile time.
+    char *args[] = {(char *)params, (char *)0};
+    return execvp(args[0], args);
 }
 
-int main(int argc, char** argv) {                                           
-  printf("Namastey from main(), Waiting for JAIL.....\n");
-  clone(jail, stack_memory(), SIGCHLD, 0);                          // Clone syscall to clone precoess (`$ man fork` for more details)
+int jail(void *args)
+{ // Function JAIL that is container
+    printf("Namastey from JAIL \n");
+    run("/bin/sh");
+    return 0;
+}
 
-  return 0;
+int main(int argc, char **argv)
+{
+    printf("Namastey from main(), Waiting for JAIL.....\n");
+    clone(jail, stack_memory(), SIGCHLD, 0);    // Clone syscall to clone precoess (`$ man fork` for more details)
+    wait(nullptr);                              // After cloning a process wait until all the child processes created to complete(CHILD process return *nullptr after completion)
+    printf("Exitting from container.... \n");
+    return 0;
 }
