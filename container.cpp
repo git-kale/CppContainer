734ead345c7aab569297aebda5e910ec57f8e512
Mahesh Kale <kjyotiba@cs.iitr.ac.in>
2019-03-10 23:33:27 +0530

    add dunction to start proc-ids from 0 and add a test dockerimage in /root

--- container.cpp
+++ container.cpp
@@ -2,6 +2,7 @@
 #include <sched.h>  // Contain clone syscall for cloning a process
 #include <unistd.h> // Contain execvp function
 #include <wait.h>   // Contain ANSI C signal handeling like SIGCHILD
+#include <sys/mount.h>
 
 char *stack_memory()
 {
@@ -17,27 +18,65 @@ char *stack_memory()
     return stack + stackSize; // As stack grows backward so we have to start from end of stack   {(end of stack)| *stack|-------> stackSize(start of stack)}
 }
 
-template <typename P>                   
+template <typename P>
 int run(P params)
 {
     //basically generating the arguments array at compile time.
     char *args[] = {(char *)params, (char *)0};
     return execvp(args[0], args);
 }
+template <typename Function>
+void clone_process(Function &&function, int flags)
+{
+    auto pid = clone(function, stack_memory(), flags, 0);
+
+    wait(nullptr);
+}
+
+void setup_vars();
+void setup_root(const char *root);
 
 int jail(void *args)
-{ // Function JAIL that is container
-    clearenv();                                 // Removes all enviroment variables s.t. no variables are passed onto child
+{
+
     printf("Namastey from JAIL \n");
-    run("/bin/sh");
+    printf("jail pid : %d\n", getpid());
+    sethostname("Devil's Nest", 13);
+    setup_vars();
+
+    setup_root("./root");
+
+    mount("proc", "/proc", "proc", 0, 0);
+
+    auto runThis = [](void *args) -> int { run("/bin/sh"); };
+
+    clone_process(runThis, SIGCHLD);
+
+    umount("/proc");
     return 0;
+}
+
+void setup_root(const char *folder)
+{
+    chroot(folder);
+    chdir("/");
+}
+
+void setup_vars()
+{
+    clearenv();
+    setenv("TERM", "xterm-256color", 0);
+    setenv("PATH", "/bin/:/sbin/:usr/bin:/usr/sbin", 0);
 }
 
 int main(int argc, char **argv)
 {
+
     printf("Namastey from main(), Waiting for JAIL.....\n");
-    clone(jail, stack_memory(), CLONE_NEWUTS | SIGCHLD, 0);    // Clone syscall to clone precoess (`$ man fork` for more details, UTS->Universal time sharing {You can have a new host under the same `Linux Namespace`})
-    wait(nullptr);                              // After cloning a process wait until all the child processes created to complete(CHILD process return *nullptr after completion)
+    printf("parent pid : %d\n", getpid());
+
+    clone_process(jail, CLONE_NEWPID | CLONE_NEWUTS | SIGCHLD);
     printf("Exitting from container.... \n");
+
     return 0;
 }
